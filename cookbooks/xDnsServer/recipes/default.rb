9a527a4f42b3280f052b5bd7e62a723ec7927568
Alan Hayes <ahayes@sei.com>
2018-08-02 10:35:07 -0400

    download wmf 5.1

--- cookbooks/xDnsServer/recipes/default.rb
+++ cookbooks/xDnsServer/recipes/default.rb
@@ -5,3 +5,4 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 include_recipe 'xDnsServer::git'
+include_recipe 'xDnsServer::wmf'
---
+++ cookbooks/xDnsServer/recipes/wmf.rb
@@ -0,0 +1,6 @@
+
+
+remote_file 'C:\Users\Public\Downloads\Win8.1AndW2K12R2-KB3191564-x64.msu' do
+  source 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu'
+  action :create
+end
