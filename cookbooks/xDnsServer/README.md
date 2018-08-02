cbf191bf0567cfe2728a02057fd76fa23e572744
Alan Hayes <ahayes@sei.com>
2018-08-02 08:53:09 -0400

    kitchen.yml for windows server 2012 opentable

---
+++ cookbooks/xDnsServer/README.md
@@ -0,0 +1,4 @@
+# xDnsServer
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/xDnsServer/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: xDnsServer
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
