a81012868ec5f95726e41fd949a12be88abcdaab
Alan Hayes <ahayes@sei.com>
2018-08-02 10:49:28 -0400

    wusa update utility update 0s

--- cookbooks/xDnsServer/recipes/wmf.rb
+++ cookbooks/xDnsServer/recipes/wmf.rb
@@ -3,4 +3,11 @@
 remote_file 'C:\Users\Public\Downloads\Win8.1AndW2K12R2-KB3191564-x64.msu' do
   source 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu'
   action :create
+  notifies :run, 'execute[wusa_wmf5_1]', :immediately
+end
+
+
+execute 'wusa_wmf5_1' do
+  command 'wusa.exe "C:\Users\Public\Downloads\Win8.1AndW2K12R2-KB3191564-x64.msu" /quiet'
+  action :nothing
 end
