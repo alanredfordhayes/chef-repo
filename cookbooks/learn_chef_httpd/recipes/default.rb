830e083ddf3afa4bafcd638398b05862edbf337a
Alan Redford Hayes <alanredfordhayes@gmail.com>
2020-02-04 12:34:50 -0500

    update default.rb

--- cookbooks/learn_chef_httpd/recipes/default.rb
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+    action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+    source 'index.html.erb'
+end
