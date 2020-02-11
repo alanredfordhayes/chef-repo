9768c55dfda579fc42430c0823d01c1b97794a7b
Alan Redford Hayes <alanredfordhayes@gmail.com>
2020-02-11 10:52:04 -0500

    download centos iso to server

--- cookbooks/rhcsa/recipes/default.rb
+++ cookbooks/rhcsa/recipes/default.rb
@@ -3,3 +3,31 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+directory '/study' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+end
+
+template '/study/goodfile' do
+    source 'goodfile.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+end
+
+template '/study/badfile' do
+    source 'badfile.erb'
+    owner 'root'
+    owner 'root'
+    mode '0755'
+end
+
+remote_file '/study/CentOS-8.1.1911-x86_64-dvd1.iso' do
+    source 'http://mirror.math.princeton.edu/pub/centos/8.1.1911/isos/x86_64/CentOS-8.1.1911-x86_64-dvd1.iso'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end
